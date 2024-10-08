369fe33d952a889e0f3137df41596367f6409055
Xianting Lu <38641973+xiantinglu@users.noreply.github.com>
2024-10-08 01:33:35 +0000

    modify task2.sh

--- task2.sh
+++ task2.sh
@@ -2,4 +2,4 @@
 
 grep -l "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s*[3-9]\d*|[1-9]\d{2,} d" | \
 gawk '{lines[NR] = $0} END {for (i = asort(lines); i >= 1; i--) print lines[i]}' | \
-sed 's/file_/filtered_/' | cut -d: -f1 | cut -d' ' -f8
+sed 's/file_/filtered_/' | cut -d: -f1 | cut -d' ' -f8 | cut -d'/' -f2
