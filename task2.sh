996b50fc0f71f279b6a73b6feaba31cfb45df3d8
Xianting Lu <38641973+xiantinglu@users.noreply.github.com>
2024-10-22 15:12:45 +0000

    modify files

--- task2.sh
+++ task2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-grep -l "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s*[3-9]\d*|[1-9]\d{2,} d" | \
+grep -l "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s*([3-9])|([1-9][0-9]{1,}) d" | \
 gawk '{lines[NR] = $0} END {for (i = asort(lines); i >= 1; i--) print lines[i]}' | \
 sed 's/file_/filtered_/' | cut -d: -f1 | cut -d' ' -f8 | cut -d'/' -f2
+
--- task3.sh
+++ task3.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-grep -E "^[0-9]+,[0-9]+,2,.*,S\s$" /workspaces/hw4/titanic.csv | sed 's/female/F/' | sed 's/male/M/' | gawk -F, '{sum += $7; count++} END {print sum/count}'
+# grep -E "^[0-9]+,[0-9]+,2,.*,S\s$" /workspaces/hw4/titanic.csv | sed 's/female/F/' | sed 's/male/M/' | gawk -F, '{sum += $7; count++} END {print sum/count}'
+
+gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/' /workspaces/hw4/titanic.csv | \
+sed 's/female/F/' | sed 's/male/M/' | \
+gawk -F, '$7 != "" {sum += $7; count++} END {if (count > 0) print sum/count}'
