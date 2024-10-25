f7c76d570a9d2316318498405222ce27f8b294f1
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-10-25 14:51:06 -0400

    Update task3.sh dir

--- task3.sh
+++ task3.sh
@@ -2,6 +2,6 @@
 
 # grep -E "^[0-9]+,[0-9]+,2,.*,S\s$" /workspaces/hw4/titanic.csv | sed 's/female/F/' | sed 's/male/M/' | gawk -F, '{sum += $7; count++} END {print sum/count}'
 
-gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/' /workspaces/hw4/titanic.csv | \
+gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/' titanic.csv | \
 sed 's/female/F/' | sed 's/male/M/' | \
 gawk -F, '$7 != "" {sum += $7; count++} END {if (count > 0) print sum/count}'
