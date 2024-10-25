b7b5b9ea70871631cc1b0742d81ae58cdb27e08f
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-10-25 14:34:04 -0400

    Update correct style task2.sh

--- task2.sh
+++ task2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -l "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s*([3-9])|([1-9][0-9]{1,}) d" | \
+grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s*([3-9])|([1-9][0-9]{1,}) d" | \
 gawk '{lines[NR] = $0} END {for (i = asort(lines); i >= 1; i--) print lines[i]}' | \
-sed 's/file_/filtered_/' | cut -d: -f1 | cut -d' ' -f8 | cut -d'/' -f2
+gawk '{print $2}' | cut -d":" -f1 | sed 's/file_/filtered_/'
 
