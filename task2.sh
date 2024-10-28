9e3fa0e4e24293c37218ebf5e193edb1cb38e0bc
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-10-28 19:11:02 -0400

    debug task2.sh

--- task2.sh
+++ task2.sh
@@ -2,5 +2,5 @@
 
 grep -l "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | \
 grep -E "^\s*([3-9])|([1-9][0-9]{1,}) d" | \
-gawk '{lines[NR] = $0} END {for (i = asort(lines); i >= 1; i--) print lines[i]}' | \
-sed 's/file_/filtered_/' 
+gawk '{count = $1; file = $2; sub(/:CSC510$/, "", file); "stat -c%s " file | getline size; print count, size, file; }' | \
+sort -k1,1nr -k2,2nr | sed 's/file_/filtered_/' 
